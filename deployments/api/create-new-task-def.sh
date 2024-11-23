3c8a6e3973b740033fbeb2a76d73cae51f82d8cc
Erotokritos Avramidis <erotokritos.a22@gmail.com>
2024-11-23 05:09:35 +0200

    added environments

--- deployments/api/create-new-task-def.sh
+++ deployments/api/create-new-task-def.sh
@@ -21,23 +21,14 @@ LATEST_TASK_DEF=$(aws ecs describe-task-definition --task-definition "$TASK_FAMI
 CONTAINER_DEFINITIONS=$(echo "$LATEST_TASK_DEF" | jq ".taskDefinition.containerDefinitions")
 TASK_ROLE_ARN=$(echo "$LATEST_TASK_DEF" | jq ".taskDefinition.taskRoleArn")
 EXECUTION_ROLE_ARN=$(echo "$LATEST_TASK_DEF" | jq ".taskDefinition.executionRoleArn")
-CPU=$(echo "$LATEST_TASK_DEF" | jq ".taskDefinition.cpu")
-MEMORY=$(echo "$LATEST_TASK_DEF" | jq ".taskDefinition.memory")
+NETWORK_MODE=$(echo "$LATEST_TASK_DEF" | jq ".taskDefinition.networkMode")
 
 echo "Updating the container definition for container: $CONTAINER_NAME with new image: $NEW_IMAGE"
-UPDATED_CONTAINER_DEFINITIONS=$(echo "$CONTAINER_DEFINITIONS" | jq --arg cn "$CONTAINER_NAME" --arg ni "$NEW_IMAGE" '
+UPDATED_CONTAINER_DEFINITIONS=$(echo "$CONTAINER_DEFINITIONS" | jq -c --arg cn "$CONTAINER_NAME" --arg ni "$NEW_IMAGE" '
   map(if .name == $cn then .image = $ni else . end)
 ')
 
 echo "Registering the new task definition..."
-NEW_TASK_DEF_ARN=$(aws ecs register-task-definition \
-  --family "$TASK_FAMILY" \
-  --task-role-arn "$TASK_ROLE_ARN" \
-  --execution-role-arn "$EXECUTION_ROLE_ARN" \
-  --network-mode "$NETWORK_MODE" \
-  --container-definitions "$UPDATED_CONTAINER_DEFINITIONS" \
-  --cpu "$CPU" \
-  --memory "$MEMORY" \
-  --query "taskDefinition.taskDefinitionArn" --output text)
+NEW_TASK_DEF_ARN=$(aws ecs register-task-definition --family $TASK_FAMILY --task-role-arn $TASK_ROLE_ARN --execution-role-arn "$EXECUTION_ROLE_ARN" --network-mode $NETWORK_MODE --container-definitions $UPDATED_CONTAINER_DEFINITIONS --query "taskDefinition.taskDefinitionArn" --output text)
 
 echo "New task definition registered successfully: $NEW_TASK_DEF_ARN"
